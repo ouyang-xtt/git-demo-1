13786905e27a4a91d6615f258d5da6e8863b34f7
ouyangzhiji <310561198@qq.com>
2022-04-16 21:44:27 +0800

    第二次提交测试

--- src/main/java/com/ouyang/gitTest.java
+++ src/main/java/com/ouyang/gitTest.java
@@ -3,5 +3,6 @@ package com.ouyang;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("git-test---------");
+        System.out.println("git-test2---------");
     }
 }
